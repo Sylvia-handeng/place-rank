9064bb4a5b4d3969cd2968d94b0253f280877e7e
Han Deng <eve2daisy1@gmail.com>
2017-12-04 00:25:51 -0500

    Update getInfo.py

--- getInfo.py
+++ getInfo.py
@@ -33,12 +33,20 @@ allRes={}
 allRes['name']=[]
 allRes['lat']=[]
 allRes['lon']=[]
-allRes['gopen_now']=[]
 allRes['vicinity']=[]
 allRes['types']=[]
 allRes['url']=[]
-allRes['pop']=[]
 allRes['open_days']=[]
+
+allRes['pop_numbers']=[]
+allRes['pop_info']=[]
+
+allRes['g_open_now']=[]
+allRes['g_rating']=[]
+# allRes['g_#reviews']=[]
+allRes['y_open_now']=[]
+allRes['y_rating']=[]
+allRes['y_#reviews']=[]
 
 co=['#FF3368','#FF8D33','#FFC433','#A5FF33','#33CEFF','#3364FF','#A233FF']
 
@@ -56,18 +64,20 @@ def getAllGPS():
 coorlist=getAllGPS()
 driver = webdriver.Chrome()
 
-def getIndexes(url,open_days): # grab past value from mon-sun,6am-12am,18 values a day and current live value	
-	res = {}
-	res['time']=[]
+def getIndexes(url,open_days): # grab past value from mon-sun,6am-11pm,18 values a day and current live value	
+	numbers = {}
+	info={}
+	numbers['time']=[]
 	for i in range(len(open_days)):
 		if open_days[i]==0:
-			res['Sun']=[]
+			numbers['Sun']=[]
 		else:
-			res[str(calendar.day_abbr[int(open_days[i])-1])]=[]
+			numbers[str(calendar.day_abbr[int(open_days[i])-1])]=[]
 
 	value = []
 	live_tex = []
 	live_value=[]
+	reviews=[]
 
 	times=[]
 	valuess=[]
@@ -78,11 +88,14 @@ def getIndexes(url,open_days): # grab past value from mon-sun,6am-12am,18 values
 	value = driver.find_elements_by_class_name("section-popular-times-bar")
 	live_tex = driver.find_elements_by_class_name("section-popular-times-live-description")
 	live_value = driver.find_elements_by_class_name("section-popular-times-live-value")
+	reviews=driver.find_element_by_xpath('//*[@id="pane"]/div/div[2]/div/div/div[1]/div[3]/div[2]/div/div[1]/span[3]/ul/li/span[2]/span[1]/button')
+	info['g_#reviews']=reviews.text
+
 	if len(live_tex) != 0:
-		res['live_text'] = live_tex[0].text
+		info['live_text'] = live_tex[0].text
 		# text is an object of webElemenet
 	if len(live_value) != 0:
-		res['live_value'] = live_value[0].get_attribute("aria-label")
+		info['live_value'] = live_value[0].get_attribute("aria-label")
 	
 	for ele in range(len(value)):
 		classname = value[ele].get_attribute("class")
@@ -92,16 +105,17 @@ def getIndexes(url,open_days): # grab past value from mon-sun,6am-12am,18 values
 				times.append(re.sub(r' ','',index_list[1]))
 				valuess.append(float(re.sub(r'%','',index_list[0])))
 			if "Current" in str(value[ele].get_attribute("aria-label")):
-				res['current_value'] = value[ele].get_attribute("aria-label") 
+				info['current_value'] = value[ele].get_attribute("aria-label") 
 	
-	res['time'].append([list(Counter(times).keys())[i].split('.')[0] for i in range(len(list(Counter(times).keys())))])
+	numbers['time'].append([list(Counter(times).keys())[i].split('.')[0] for i in range(len(list(Counter(times).keys())))])
 	
 	for i in range(len(times)): #attach values to weekdays at the order from place detail API
 		appeared=list(Counter(times[:i+1]).values())[list(Counter(times[:i+1]).keys()).index(times[i])]
-		res[list(res.keys())[appeared]].append(valuess[i])
+		numbers[list(numbers.keys())[appeared]].append(valuess[i])
 
-	print(res)			
-	return res
+	# print(numbers)
+	# print(info)			
+	return[numbers,info]
 	
 for a in range(len(coorlist)):
 	page=requests.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+coorlist[a]+"&radius=55&keyword=restaurant&key=AIzaSyCs9iFQSqVfgFtPhey4WB-zwHd5hhJPeYs")#key1=AIzaSyA4ZJTg8gVk9Dg4O_nI3LDCeXrZwXfaU2c  key2=AIzaSyCs9iFQSqVfgFtPhey4WB-zwHd5hhJPeYs
@@ -126,33 +140,41 @@ for a in range(len(coorlist)):
 			allRes['lon'].append(stuff3['results'][b]['geometry']['location']['lng'])
 			allRes['vicinity'].append(stuff3['results'][b]['vicinity'])
 			allRes['types'].append(stuff3['results'][b]['types'])
+			allRes['g_rating'].append(stuff3['results'][b]['rating'])
 			for c in range(len(stu3['result']['opening_hours']['periods'])):
 				days_list.append(stu3['result']['opening_hours']['periods'][c]['open']['day'])
 			try:
-				allRes['gopen_now'].append(stuff3['results'][b]['opening_hours']['open_now'])
+				allRes['g_open_now'].append(stuff3['results'][b]['opening_hours']['open_now'])
 			except:
-				allRes['gopen_now'].append("N/A")
+				allRes['g_open_now'].append("N/A")
 			allRes['open_days'].append(days_list)			
 # print(allRes)
 
 for ele in range(len(allRes['url'])):
-	allRes['pop'].append(getIndexes(allRes['url'][ele],allRes['open_days'][ele]).copy())
+	temp=getIndexes(allRes['url'][ele],allRes['open_days'][ele])
+	allRes['pop_numbers'].append(temp[0].copy())
+	allRes['pop_info'].append(temp[1].copy())
 	print(str(ele+1)+" Place(s) Scraped")
 # getIndexes("https://maps.google.com/?cid=12777293506627589287") #for testing
 
 try:
     for i in range(len(allRes['name'])):
-    	query_api(allRes['name'][i],allRes['lat'][i],allRes['lon'][i])
+    	temp=query_api(allRes['name'][i],allRes['lat'][i],allRes['lon'][i])
+    	allRes['y_open_now'].append(temp['y_open_now'])
+    	allRes['y_rating'].append(temp['y_rating'])
+    	allRes['y_#reviews'].append(temp['y_#reviews'])
     	fig, ax=plt.subplots()
     	lege=[]
-    	v=list(allRes['pop'][i].keys())[1:]
+    	v=list(allRes['pop_numbers'][i].keys())[1:]
     	for j in range(len(v)):
-    		lege.append(ax.bar(np.arange(18)+float(j/10),allRes['pop'][i].get(v[j]),width=0.1,color=co[j])[0])
-    	ax.set_xticks(np.arange(18)+(len(list(allRes['pop'][i].keys())[1:])-1)/20)
-    	ax.set_xticklabels(allRes['pop'][i]['time'][0])
+    		lege.append(ax.bar(np.arange(18)+float(j/10),allRes['pop_numbers'][i].get(v[j]),width=0.1,color=co[j])[0])
+    	ax.set_xticks(np.arange(18)+(len(list(allRes['pop_numbers'][i].keys())[1:])-1)/20)
+    	ax.set_xticklabels(allRes['pop_numbers'][i]['time'][0])
     	ax.set_yticklabels(np.arange(0,120,20))
-    	ax.set_title(allRes['name'][i])
-    	ax.legend(lege,list(allRes['pop'][i].keys())[1:])
+    	ax.set_title((allRes['name'][i]+
+    		'\nYelp: '+re.search('[0-5][.][0-9]',str(allRes['y_rating'][i])).group(0)+' stars, '+re.search('\w+',str(allRes['y_#reviews'][i])).group(0)+' reviews'+
+    		'\nGoogle Maps: '+str(allRes['g_rating'][i])+' stars, '+str(allRes['pop_info'][i]['g_#reviews'])))
+    	ax.legend(lege,list(allRes['pop_numbers'][i].keys())[1:])
     	fig.set_size_inches(10, 5)
     	plt.show()
 except HTTPError as error:
